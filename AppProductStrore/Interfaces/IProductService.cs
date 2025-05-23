c7ba83445d527e108ae5e38f1b1e5922c4fccede
Alex <Ivanov-AS@115-PREPOD>
2025-05-23 09:32:19 +0700

    add models and product service

---
+++ AppProductStrore/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using AppProductStrore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProductStrore.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ObservableCollection<Product>> LoadProductsAsync();
+    }
+}
---
+++ AppProductStrore/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProductStrore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string ImagePath => $"ms-appdata:///local/Images/{Image}";
+        public string PriceFormatted => $"{Price:C}";
+    }
+}
