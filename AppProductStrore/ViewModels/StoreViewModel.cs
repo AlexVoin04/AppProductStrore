3511d50cab5faafe31c5ce2507ec3b980d28a55e
Alex <Ivanov-AS@115-PREPOD>
2025-05-23 11:25:40 +0700

    add product output

---
+++ AppProductStrore/ViewModels/StoreViewModel.cs
@@ -0,0 +1,35 @@
+﻿using AppProductStrore.Interfaces;
+using AppProductStrore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProductStrore.ViewModels
+{
+    public class StoreViewModel : BaseViewModel
+    {
+        private readonly IProductService _productService;
+
+        private ObservableCollection<Product> _products = new ObservableCollection<Product>();
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
+
+        public StoreViewModel(IProductService productService)
+        {
+            _productService = productService;
+            Task.Run(async () => await LoadProductsAsync()).GetAwaiter().GetResult();
+        }
+
+        private async Task LoadProductsAsync()
+        {
+            Products = new ObservableCollection<Product>();
+            var products = await _productService.LoadProductsAsync();
+            foreach (var product in products)
+                Products.Add(product);
+            Debug.WriteLine($"product count: {products.Count}");
+        }
+    }
+}
--- AppProductStrore/Views/StorePage.xaml.cs
+++ AppProductStrore/Views/StorePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AppProductStrore.Models;
+using AppProductStrore.Services;
+using AppProductStrore.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +14,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,9 +26,20 @@ namespace AppProductStrore.Views
     /// </summary>
     public sealed partial class StorePage : Page
     {
+        public StoreViewModel ViewModel { get; }
         public StorePage()
         {
             this.InitializeComponent();
+            ViewModel = new StoreViewModel(new ProductService());
+            DataContext = ViewModel;
+        }
+
+        private void ProductImage_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            if (sender is Image img)
+            {
+                img.Source = new BitmapImage(new Uri("ms-appdata:///local/Images/productDef.png"));
+            }
         }
     }
 }
